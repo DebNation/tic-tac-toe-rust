08f4ee63428a96fadadf0b01162a8551a31e70a5
Debiprasad Das <debiprasadxd@protonmail.com>
2025-08-23 19:25:54 +0530

    add audio for win/draw, fix is_any_number_left fn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use colored::*;
+use kira::{
+    AudioManager, AudioManagerSettings, DefaultBackend, sound::static_sound::StaticSoundData,
+};
 use rand::prelude::*;
-use std::io::{self};
+use std::{
+    io::{self},
+    time::Duration,
+};
 
 fn draw_box(box_numbers: &[String]) {
     for number in 0..box_numbers.len() {
@@ -78,13 +84,20 @@ fn check_winner(box_numbers: &[String]) -> bool {
     false
 }
 
-fn check_box_filled(box_numbers: &[String]) -> bool {
-    for number in box_numbers {
-        match number.as_str() {
-            "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" => return true,
-            _ => return false,
-        }
+fn is_any_number_left(box_numbers: &[String]) -> bool {
+    if box_numbers.contains(&"0".to_string())
+        || box_numbers.contains(&"1".to_string())
+        || box_numbers.contains(&"2".to_string())
+        || box_numbers.contains(&"3".to_string())
+        || box_numbers.contains(&"4".to_string())
+        || box_numbers.contains(&"5".to_string())
+        || box_numbers.contains(&"6".to_string())
+        || box_numbers.contains(&"7".to_string())
+        || box_numbers.contains(&"8".to_string())
+    {
+        return true;
     }
+
     return false;
 }
 fn main() {
@@ -94,30 +107,46 @@ fn main() {
     let mut box_numbers: Vec<String> = nums.iter().map(|num| num.to_string()).collect();
     let mut is_player_won = false;
 
-    while check_box_filled(&box_numbers) == true {
+    while is_any_number_left(&box_numbers) == true {
         player_x(&mut box_numbers, &mut is_player_won);
-        if is_player_won {
+        if is_player_won || !is_any_number_left(&box_numbers) {
             break;
         }
+
         player_y(&mut box_numbers, &mut is_player_won);
-        if is_player_won {
+        println!("{}", is_any_number_left(&box_numbers));
+        if is_player_won || !is_any_number_left(&box_numbers) {
             break;
         }
     }
-
-    if check_box_filled(&box_numbers) == false {
+    if is_any_number_left(&box_numbers) == false && is_player_won == false {
+        let mut manager =
+            AudioManager::<DefaultBackend>::new(AudioManagerSettings::default()).unwrap();
+        let draw_audio = StaticSoundData::from_file("assets/draw.mp3").unwrap();
         clearscreen::clear().expect("failed to clear screen");
         draw_box(&box_numbers);
-        println!("{}", format!("It's a Draw!").white().bold().italic());
+
+        println!(
+            "{}",
+            format!("It's a Draw!").white().on_black().bold().italic()
+        );
+
+        manager.play(draw_audio.clone()).unwrap();
+        std::thread::sleep(Duration::from_secs(2));
     }
 }
 
 fn player_x(box_numbers: &mut [String], is_player_won: &mut bool) {
-    while check_box_filled(&box_numbers) == true {
+    let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default()).unwrap();
+    // let enter_x_audio = StaticSoundData::from_file("assets/player_x_enter.mp3").unwrap();
+    let winner_x = StaticSoundData::from_file("assets/winner_x.mp3").unwrap();
+    while is_any_number_left(&box_numbers) == true {
         clearscreen::clear().expect("failed to clear screen");
         draw_box(&box_numbers);
 
         println!("{}", format!("Player X: Enter a number: ").italic());
+        // std::thread::sleep(Duration::from_secs(1));
+        // manager.play(enter_x_audio.clone()).unwrap();
         let mut input: String = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let parsed_input: u32 = input.trim().parse().expect("failed to parse the input");
@@ -139,7 +168,10 @@ fn player_x(box_numbers: &mut [String], is_player_won: &mut bool) {
             clearscreen::clear().expect("failed to clear screen");
             draw_box(&box_numbers);
             println!("{}", format!("Winner: PLayer X!").red().bold().italic());
+            // std::thread::sleep(Duration::from_secs(1));
+            manager.play(winner_x.clone()).unwrap();
             *is_player_won = true;
+            std::thread::sleep(Duration::from_secs(2));
             break;
         }
         break;
@@ -147,11 +179,16 @@ fn player_x(box_numbers: &mut [String], is_player_won: &mut bool) {
 }
 
 fn player_y(box_numbers: &mut [String], is_player_won: &mut bool) {
-    while check_box_filled(&box_numbers) == true {
+    let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default()).unwrap();
+    // let enter_y_audio = StaticSoundData::from_file("assets/player_y_enter.mp3").unwrap();
+    let winner_y = StaticSoundData::from_file("assets/winner_y.mp3").unwrap();
+    while is_any_number_left(&box_numbers) == true {
         clearscreen::clear().expect("failed to clear screen");
         draw_box(&box_numbers);
 
         println!("{}", format!("Player Y: Enter a number: ").italic());
+        // std::thread::sleep(Duration::from_secs(1));
+        // manager.play(enter_y_audio.clone()).unwrap();
         let mut input: String = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let parsed_input: u32 = input.trim().parse().expect("failed to parse the input");
@@ -173,9 +210,14 @@ fn player_y(box_numbers: &mut [String], is_player_won: &mut bool) {
             clearscreen::clear().expect("failed to clear screen");
             draw_box(&box_numbers);
             println!("{}", format!("Winner: PLayer Y!").blue().bold().italic());
+
+            // std::thread::sleep(Duration::from_secs(1));
+            manager.play(winner_y.clone()).unwrap();
             *is_player_won = true;
+            std::thread::sleep(Duration::from_secs(2));
             break;
         }
+
         break;
     }
 }
